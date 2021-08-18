5081de79f9d3733d667f0b910d5b02c6df478c99
Daniel Weller <56622988+danielweller-swp@users.noreply.github.com>
2021-08-18 11:19:11 +0200

    Change default message of hello-world

--- hello-world/index.js
+++ hello-world/index.js
@@ -1,4 +1,4 @@
 exports.helloWorld = (req, res) => {
-  let message = process.env.MESSAGE || 'Hello World, I have no secret.';
+  let message = process.env.MESSAGE || 'Hello World, this is the default message.';
   res.status(200).send(message);
 };
